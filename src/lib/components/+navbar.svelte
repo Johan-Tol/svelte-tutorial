c9c67e9761b941149ecefde336881ddf298ded43
Johndaboy <145539556+Johndaboy@users.noreply.github.com>
2026-03-17 11:36:43 +0100

    navbar added

---
+++ src/lib/components/+navbar.svelte
@@ -0,0 +1,29 @@
+
+<div class="navbar">
+    <a href="/">homepage</a>
+    <a href="/projects">projects</a>
+</div>
+
+<style>
+    .navbar {
+        height: 20%;
+        width: 100%;
+        margin: 0px;
+        background: #a00000;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    a {
+        border: 2px solid black;
+        border-radius: 15px;
+        background-color: white;
+        padding: 10px;
+        margin: 10px;
+        margin-left: 20px;
+        margin-right: 20px;
+        text-decoration: none;
+        color: black;
+    }
+</style>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Navbar from '$lib/components/+navbar.svelte'
+    let Text = $state('Default text');
+</script>
+
+<Navbar/>
+
+<h1>dit is om te kijken naar sveltekit hoe het werkt</h1>
+
+<div>
+    <div class="right">
+        <h3>{Text}</h3>
+    </div>
+</div>
+<input type="text" bind:value={Text}>
+<button onclick={() => Text = "Default text"}>reset</button>
+
+<style>
+    div {
+        width: 50%;
+        display: flex;
+        justify-content: center;
+    }
+
+    .right {
+        justify-content: right;
+    }
+</style>
